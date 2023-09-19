4cdd236cb4e7553529976076b047e5c99fc267bd
Rory <yourboyrory@gmail.com>
2023-09-19 11:03:29 -0400

    added varaible for java ver

--- start.sh
+++ start.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 ##Config##
-server_dir="/servers/minecraftserver/"
-java_parm="-server -Xmx12G -Xms8G -XX:+UseG1GC -jar" # -server -d64 -Xms1G -Xmx2G -XX:+UseG1GC -jar
+server_dir="./" 
+java_ver="17" # 1.8.0
+java_parm="-server -Xmx4G -Xms2G -XX:+UseG1GC -jar -Dlog4j2.formatMsgNoLookups=true" # -server -d64 -Xms2G -Xmx4G -XX:+UseG1GC -jar -Dlog4j2.formatMsgNoLookups=true
 
 ##DO NOT TOUCH##
 tput sgr0
 cd $server_dir
-  
+
 beep
-time java $java_parm $server_dir/spigot*.jar nogui
+time /usr/lib/jvm/jre-$java_ver/bin/java $java_parm $server_dir/spigot*.jar nogui
